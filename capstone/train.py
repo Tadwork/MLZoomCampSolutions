57efe19a4ea4967c1873172e5e4eb58ed603eeef
Tzvi Friedman <tadwork@gmail.com>
2024-01-15 16:52:25 -0500

    update model-checkpoints dir

--- capstone/train.py
+++ capstone/train.py
@@ -2,88 +2,123 @@
 Training the final model
 Saving it to a file (e.g. pickle)
 """
+import os
 import json
 
-import numpy as np
+from sklearn.model_selection import train_test_split
 import pandas as pd
+from keras.preprocessing.image import ImageDataGenerator
 import tensorflow as tf
 from tensorflow import keras
-import pickle
+from tensorflow.keras.applications import InceptionResNetV2
+from tensorflow.keras.applications.inception_resnet_v2 import preprocess_input as preprocess
 
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import KFold, train_test_split
-from sklearn.feature_extraction import DictVectorizer
-from sklearn.metrics import mean_squared_error,r2_score
-SEED = 65
-n_splits = 5
+SEED=42
 
-def train(df_train, C=1.0):
-    # dv = DictVectorizer()
-    # df_train, y_train = prepare(df_train)
-    # dicts = df_train.to_dict(orient='records')
-    # X_train = dv.fit_transform(dicts)
+def create_model(learning_rate, size_inner,droprate,  shape = (150,150,3)):
+    inception = InceptionResNetV2(include_top=False, weights='imagenet')
+
+    inception.trainable = False
+
+    inputs = keras.Input(shape=shape)
+
+    x = preprocess(inputs)
+    x = inception(x, training=False)
+    x = keras.layers.GlobalAveragePooling2D()(x)
     
-    # model = LinearRegression()
-    # model.fit(X_train, y_train)
+    x = keras.layers.Dense(size_inner, activation='relu')(x)
+    x = keras.layers.Dropout(droprate)(x)
     
-    return model, dv
+    outputs = keras.layers.Dense(3, activation='softmax')(x)
 
-def predict(df_test, dv, model):
-    # df_test, y_test = prepare(df_test)
-    # dicts = df_test.to_dict(orient='records')
-    # X_val = dv.transform(dicts)
+    model = keras.Model(inputs=inputs, outputs=outputs)
+
+    optimizer = tf.keras.optimizers.legacy.Adam(
+        learning_rate=learning_rate
+    )
+    loss = keras.losses.CategoricalCrossentropy(
+        from_logits=False,
+        label_smoothing=0.0,
+        axis=-1,
+        reduction="sum_over_batch_size",
+        name="categorical_crossentropy",
+    )
+    model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])
+    return model
+
+def train(train_dir, df_val, df_train, model,epochs=10):
+    checkpoint = keras.callbacks.ModelCheckpoint(
+        'model-checkpoints/inception_{epoch:02d}_{val_accuracy:.3f}.hdf5',
+        save_best_only=True,
+        monitor='val_accuracy',
+        mode='max'
+    )
+    train_datagen = ImageDataGenerator(
+        horizontal_flip=True,
+    )
+    target_size = (150,150)
+
+    batch_size = 32
+
+    generator_args = dict(
+        directory=train_dir,
+        x_col='ID',
+        y_col='Class',
+        class_mode='categorical',
+        batch_size=batch_size,
+        target_size=target_size
+    )
+    # Training data generator
+    train_generator = train_datagen.flow_from_dataframe(
+        df_train,
+        **generator_args
+    )
+
+    # Validation data generator
+    val_datagen = ImageDataGenerator()
+
+    val_generator = val_datagen.flow_from_dataframe(
+        df_val,
+        **generator_args
+    )
+    model.fit(train_generator,
+        epochs=epochs,
+        validation_data=val_generator,
+        callbacks=[checkpoint]
+    )
+    return model
     
-    # y_pred = model.predict(X_val)
+def validate(train_dir,model, df_val, batch_size=32):
+    val_datagen = ImageDataGenerator()
+    target_size = (150,150)
+    generator_args = dict(
+        directory=train_dir,
+        x_col='ID',
+        y_col='Class',
+        class_mode='categorical',
+        batch_size=batch_size,
+        target_size=target_size
+    )
+    val_generator = val_datagen.flow_from_dataframe(
+        df_val,
+        **generator_args
+    )
+    return model.evaluate(val_generator)
     
-    return y_pred, y_test
-
-def validate(df_full_train):
-    # validation
-    print('-------------------')
-    print(f'doing validation')
-    kfold = KFold(n_splits=n_splits, shuffle=True)
-    scores = []
-    fold = 0
-    for train_idx, val_idx in kfold.split(df_full_train):
-        df_train = df_full_train.iloc[train_idx]
-        df_val = df_full_train.iloc[val_idx]
-
-        model,dv = train(df_train)
-        y_pred,y_val = predict(df_val, dv, model)
-
-        r2 = r2_score(y_val, y_pred)
-        scores.append(r2)
-
-        print(f'r2 on fold {fold} is {r2}')
-        fold = fold + 1
-    print('validation results:')
-    print('%.3f +- %.3f' % ( np.mean(scores), np.std(scores)))
-    print('-------------------')
-
-def convert_to_lite(model):
-    converter = tf.lite.TFLiteConverter.from_keras_model(model)
-
-    tflite_model = converter.convert()
-
-    with open('face-age-predict.tflite', 'wb') as f_out:
-        f_out.write(tflite_model)
-        
+    
 if __name__ == '__main__':
-    data = pd.read_csv('./data/amazon_laptop_prices_v01.csv')
-    data = clean.clean_data(data)
-    df_train_full, df_test = train_test_split(data, test_size=0.2,random_state=SEED)
-    model, dv = train(df_train_full)
-    y_pred, y_test = predict(df_test, dv, model)
-    rmse = mean_squared_error(y_test, y_pred, squared=False)
-    r2 = r2_score(y_test, y_pred)
-    print(f'RMSE: {rmse:.3f}')
-    print(f'R2: {r2:.2f}')
-    validate(df_train_full)
-    with open('model.bin', 'wb') as model_out:
-        pickle.dump(model, model_out)
-    with open('dv.bin', 'wb') as dv_out:
-        pickle.dump(dv, dv_out)
-    print('successfully saved model and dv')
-    sample_input = data.to_dict(orient='records')[0]
-    del sample_input['price']
-    print(f'sample inference input {json.dumps(sample_input, indent=2)}')
+    current_directory = os.getcwd()
+    labels_df = pd.read_csv(current_directory + '/face-age-detection/train.csv')
+    df_train, df_test = train_test_split(labels_df, test_size=0.4, random_state=SEED)
+    df_test, df_val = train_test_split(df_test, test_size=0.5, random_state=SEED)
+
+    df_test = df_test.reset_index(drop=True)
+    df_val = df_val.reset_index(drop=True)
+    df_train = df_train.reset_index(drop=True)
+    
+    train_dir = current_directory + '/face-age-detection/Train/'
+    model = create_model(learning_rate=0.0001, size_inner=64,droprate=0.5)
+    trained_model = train(train_dir, df_val, df_train, model,epochs=10)
+    loss, accuracy = validate(train_dir,trained_model, df_val, batch_size=32)
+    model.save('model.hdf5')
+    print('successfully saved model and weights')
